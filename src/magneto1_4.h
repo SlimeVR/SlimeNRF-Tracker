c070b6c5ad67556eb2318a7d34f3e36768e2543e
digital <jcburnside97@gmail.com>
2023-06-10 15:22:49 -0400

    added missing include gaurds

--- src/magneto1_4.h
+++ src/magneto1_4.h
@@ -1,2 +1,5 @@
+#ifndef MAGNETO1_4_H
+#define MAGNETO1_4_H
 void magneto_sample(double x, double y, double z, double* ata, double* norm_sum, double* sample_count);
 void magneto_current_calibration(float BAinv[4][3], double* ata, double norm_sum, double sample_count);
+#endif
