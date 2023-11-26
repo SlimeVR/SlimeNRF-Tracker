9571fafd6296353209fee52f53b12dd711c8274f
sctanf <36978460+sctanf@users.noreply.github.com>
2023-11-26 12:09:02 -0600

    appease warnings

--- src/main.c
+++ src/main.c
@@ -217,59 +217,6 @@ bool send_data = false;
 uint16_t led_clock = 0;
 uint32_t led_clock_offset = 0;
 
-static void timer_handler(nrf_timer_event_t event_type, void *p_context) {
-	if (event_type == NRF_TIMER_EVENT_COMPARE1 && esb_state == true) {
-		if (last_reset < LAST_RESET_LIMIT) {
-			last_reset++;
-			if (send_data) { // scuffed check
-				esb_write_payload(&tx_payload); // Add transmission to queue
-				esb_start_tx();
-				send_data = false;
-			}
-//			esb_flush_tx();
-		} else {
-			esb_disable();
-			esb_initialize_rx();
-			esb_start_rx();
-			esb_state = false;
-			nrfx_timer_pause(&m_timer);
-			timer_state = false;
-			LOG_INF("timer reset elapsed");
-		}
-	} else if (event_type == NRF_TIMER_EVENT_COMPARE2 && esb_state == true) {
-		esb_disable();
-		esb_initialize_rx();
-		esb_start_rx();
-		esb_state = false;
-	} else if (event_type == NRF_TIMER_EVENT_COMPARE3 && esb_state == false) {
-		esb_stop_rx();
-		esb_disable();
-		esb_initialize();
-		esb_state = true;
-	}
-}
-
-void timer_init(void) {
-	nrfx_err_t err;
-	nrfx_timer_config_t timer_cfg = NRFX_TIMER_DEFAULT_CONFIG(1000000);  
-	//timer_cfg.frequency = NRF_TIMER_FREQ_1MHz;
-	//timer_cfg.mode = NRF_TIMER_MODE_TIMER;
-	//timer_cfg.bit_width = NRF_TIMER_BIT_WIDTH_16;
-	//timer_cfg.interrupt_priority = NRFX_TIMER_DEFAULT_CONFIG_IRQ_PRIORITY;
-	//timer_cfg.p_context = NULL;
-	nrfx_timer_init(&m_timer, &timer_cfg, timer_handler);
-	uint32_t ticks = nrfx_timer_ms_to_ticks(&m_timer, 3);
-	nrfx_timer_extended_compare(&m_timer, NRF_TIMER_CC_CHANNEL0, ticks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, false);
-	LOG_INF("timer at %d", ticks * (paired_addr[1]*2 + 3) / 21); // TODO: temp set max 8
-	nrfx_timer_compare(&m_timer, NRF_TIMER_CC_CHANNEL1, ticks * (paired_addr[1]*2 + 3) / 21, true); // timeslot to send data  TODO: temp set max 8
-	nrfx_timer_compare(&m_timer, NRF_TIMER_CC_CHANNEL2, ticks * 19 / 21, true); // switch to rx
-	nrfx_timer_compare(&m_timer, NRF_TIMER_CC_CHANNEL3, ticks * 2 / 21, true); // switch to tx
-	nrfx_timer_enable(&m_timer);
-	IRQ_DIRECT_CONNECT(TIMER1_IRQn, 0, nrfx_timer_1_irq_handler, 0);
-	irq_enable(TIMER1_IRQn);
-	timer_state = true;
-}
-
 void event_handler(struct esb_evt const *event)
 {
 	switch (event->evt_id)
@@ -442,6 +389,59 @@ int esb_initialize_rx(void)
 	return 0;
 }
 
+static void timer_handler(nrf_timer_event_t event_type, void *p_context) {
+	if (event_type == NRF_TIMER_EVENT_COMPARE1 && esb_state == true) {
+		if (last_reset < LAST_RESET_LIMIT) {
+			last_reset++;
+			if (send_data) { // scuffed check
+				esb_write_payload(&tx_payload); // Add transmission to queue
+				esb_start_tx();
+				send_data = false;
+			}
+//			esb_flush_tx();
+		} else {
+			esb_disable();
+			esb_initialize_rx();
+			esb_start_rx();
+			esb_state = false;
+			nrfx_timer_pause(&m_timer);
+			timer_state = false;
+			LOG_INF("timer reset elapsed");
+		}
+	} else if (event_type == NRF_TIMER_EVENT_COMPARE2 && esb_state == true) {
+		esb_disable();
+		esb_initialize_rx();
+		esb_start_rx();
+		esb_state = false;
+	} else if (event_type == NRF_TIMER_EVENT_COMPARE3 && esb_state == false) {
+		esb_stop_rx();
+		esb_disable();
+		esb_initialize();
+		esb_state = true;
+	}
+}
+
+void timer_init(void) {
+//	nrfx_err_t err;
+	nrfx_timer_config_t timer_cfg = NRFX_TIMER_DEFAULT_CONFIG(1000000);  
+	//timer_cfg.frequency = NRF_TIMER_FREQ_1MHz;
+	//timer_cfg.mode = NRF_TIMER_MODE_TIMER;
+	//timer_cfg.bit_width = NRF_TIMER_BIT_WIDTH_16;
+	//timer_cfg.interrupt_priority = NRFX_TIMER_DEFAULT_CONFIG_IRQ_PRIORITY;
+	//timer_cfg.p_context = NULL;
+	nrfx_timer_init(&m_timer, &timer_cfg, timer_handler);
+	uint32_t ticks = nrfx_timer_ms_to_ticks(&m_timer, 3);
+	nrfx_timer_extended_compare(&m_timer, NRF_TIMER_CC_CHANNEL0, ticks, NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK, false);
+	LOG_INF("timer at %d", ticks * (paired_addr[1]*2 + 3) / 21); // TODO: temp set max 8
+	nrfx_timer_compare(&m_timer, NRF_TIMER_CC_CHANNEL1, ticks * (paired_addr[1]*2 + 3) / 21, true); // timeslot to send data  TODO: temp set max 8
+	nrfx_timer_compare(&m_timer, NRF_TIMER_CC_CHANNEL2, ticks * 19 / 21, true); // switch to rx
+	nrfx_timer_compare(&m_timer, NRF_TIMER_CC_CHANNEL3, ticks * 2 / 21, true); // switch to tx
+	nrfx_timer_enable(&m_timer);
+	IRQ_DIRECT_CONNECT(TIMER1_IRQn, 0, nrfx_timer_1_irq_handler, 0);
+	irq_enable(TIMER1_IRQn);
+	timer_state = true;
+}
+
 int powerstate = 0;
 int last_powerstate = 0;
 int mag_level = 0;
@@ -611,7 +611,7 @@ void main_imu_thread(void) {
 			float az = a[2] - accelBias[2];
 
 #if MAG_ENABLED
-			float mx, my, mz;
+			float mx = 0, my = 0, mz = 0;
 			if (last_powerstate == 0) {
 				float m[3];
 				mmc_mag_read(main_mag, m);
@@ -922,10 +922,10 @@ void power_check(void) {
 		gpio_pin_set_dt(&led, 0); // Turn off LED
 		configure_system_off_dock(); // usually charging, i would flash LED but that will drain the battery while it is charging..
 	}
-	LOG_INF("Battery %u\% (%dmV)", batt_pptt/100, batt_mV);
+	LOG_INF("Battery %u%% (%dmV)", batt_pptt/100, batt_mV);
 }
 
-void main(void)
+int main(void)
 {
 	int32_t reset_reason = NRF_POWER->RESETREAS;
 	NRF_POWER->RESETREAS = NRF_POWER->RESETREAS; // Clear RESETREAS
