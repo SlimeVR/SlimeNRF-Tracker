c41854eef980d0d04c0dff88f14ef18c4e6a4072
sctanf <36978460+sctanf@users.noreply.github.com>
2024-10-21 20:05:19 -0500

    add back basic console

---
+++ src/console.c
@@ -0,0 +1,97 @@
+#include "globals.h"
+#include "system.h"
+
+#define USB DT_NODELABEL(usbd)
+#if DT_NODE_HAS_STATUS(USB, okay)
+
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/class/usb_hid.h>
+#include <zephyr/console/console.h>
+#include <zephyr/sys/reboot.h>
+
+static bool configured;
+static const struct device *hdev;
+static ATOMIC_DEFINE(hid_ep_in_busy, 1);
+
+#define HID_EP_BUSY_FLAG 0
+
+LOG_MODULE_REGISTER(console, LOG_LEVEL_INF);
+
+static void int_in_ready_cb(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+	if (!atomic_test_and_clear_bit(hid_ep_in_busy, HID_EP_BUSY_FLAG)) {
+		LOG_WRN("IN endpoint callback without preceding buffer write");
+	}
+}
+
+static void status_cb(enum usb_dc_status_code status, const uint8_t *param)
+{
+	switch (status) {
+	case USB_DC_RESET:
+		configured = false;
+		break;
+	case USB_DC_CONFIGURED:
+		if (!configured) {
+			int_in_ready_cb(hdev);
+			configured = true;
+		}
+		break;
+	case USB_DC_SOF:
+		break;
+	default:
+		LOG_DBG("status %u unhandled", status);
+		break;
+	}
+}
+
+void console_thread(void)
+{
+	usb_enable(status_cb);
+
+	console_getline_init();
+	printk("*** " CONFIG_USB_DEVICE_MANUFACTURER " " CONFIG_USB_DEVICE_PRODUCT " ***\n");
+	printk("reboot                       Soft reset the device\n");
+	printk("calibrate                    Calibrate sensor ZRO\n");
+	printk("pair                         Clear pairing data\n");
+	printk("dfu                          Enter DFU bootloader\n");
+
+	uint8_t command_reboot[] = "reboot";
+	uint8_t command_calibrate[] = "calibrate";
+	uint8_t command_pair[] = "pair";
+	uint8_t command_dfu[] = "dfu";
+
+	while (1) {
+		uint8_t *line = console_getline();
+
+		if (memcmp(line, command_reboot, sizeof(command_reboot)) == 0)
+		{
+			sys_reboot(SYS_REBOOT_COLD);
+		}
+		else if (memcmp(line, command_calibrate, sizeof(command_calibrate)) == 0)
+		{
+			reboot_counter_write(101);
+			k_msleep(1);
+			sys_reboot(SYS_REBOOT_WARM);
+		}
+		else if (memcmp(line, command_pair, sizeof(command_pair)) == 0) 
+		{
+			reboot_counter_write(102);
+			k_msleep(1);
+			sys_reboot(SYS_REBOOT_WARM);
+		}
+		else if (memcmp(line, command_dfu, sizeof(command_dfu)) == 0)
+		{
+			NRF_POWER->GPREGRET = 0x57;
+			sys_reboot(SYS_REBOOT_COLD);
+		}
+		else
+		{
+			printk("Unknown command\n");
+		}
+	}
+}
+
+K_THREAD_DEFINE(console_thread_id, 512, console_thread, NULL, NULL, NULL, 6, 0, 0);
+
+#endif
