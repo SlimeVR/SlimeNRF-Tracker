d4d44c37c426b8f898b2ecf64c8323fe3ecd50ea
sctanf <36978460+sctanf@users.noreply.github.com>
2024-10-21 21:22:47 -0500

    use kconfig for some globals

--- src/globals.h
+++ src/globals.h
@@ -5,14 +5,14 @@
 
 #include "retained.h"
 
-#define USER_SHUTDOWN_ENABLED true // Allow user to use reset or sw0 to shutdown
-#define MAG_ENABLED true // Use magnetometer if it is present
-#define IGNORE_RESET true // If sw0 available, don't change any reset behavior
-//#define WOM_USE_DCDC true // Use DCDC instead of LDO for WOM if it is more efficient
+#define USER_SHUTDOWN_ENABLED CONFIG_USER_SHUTDOWN // Allow user to use reset or sw0 to shutdown
+#define MAG_ENABLED CONFIG_USE_MAG // Use magnetometer if it is present
+#define IGNORE_RESET CONFIG_IGNORE_RESET // If sw0 available, don't change any reset behavior
+#define WOM_USE_DCDC CONFIG_WOM_USE_DCDC // Use DCDC instead of LDO for WOM if it is more efficient
 
-#define SENSOR_GYROSCOPE_AXES_ALIGNMENT gx, -gz, gy // gyro alignment
-#define SENSOR_ACCELEROMETER_AXES_ALIGNMENT ax, -az, ay // accel alignment
-#define SENSOR_MAGNETOMETER_AXES_ALIGNMENT my, mz, -mx // mag alignment
-#define SENSOR_QUATERNION_CORRECTION 0.5f, -0.5f, -0.5f, -0.5f // correction quat
+#define SENSOR_GYROSCOPE_AXES_ALIGNMENT gx, -gz, gy // gyro axes alignment to sensor body
+#define SENSOR_ACCELEROMETER_AXES_ALIGNMENT ax, -az, ay // accel axes alignment to sensor body
+#define SENSOR_MAGNETOMETER_AXES_ALIGNMENT my, mz, -mx // mag axes alignment to sensor body
+#define SENSOR_QUATERNION_CORRECTION 0.5f, -0.5f, -0.5f, -0.5f // correction quat for sensor to mounting orientation
 
 #endif
