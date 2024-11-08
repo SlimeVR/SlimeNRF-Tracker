fbc793e1d32d9d30ca4ea89d2ea869942ebfaf43
sctanf <36978460+sctanf@users.noreply.github.com>
2024-11-08 00:00:46 -0600

    comment led colors and indications

--- src/system/led.h
+++ src/system/led.h
@@ -23,25 +23,25 @@ enum sys_led_pattern {
 	SYS_LED_PATTERN_OFF_FORCE, // ignores lower priority patterns
 
 	SYS_LED_PATTERN_OFF, // yield to lower priority patterns
-	SYS_LED_PATTERN_ON,
-	SYS_LED_PATTERN_SHORT, // 100ms on 900ms off
-	SYS_LED_PATTERN_LONG, // 500ms on 500ms off
-	SYS_LED_PATTERN_FLASH, // 200ms on 200ms off
+	SYS_LED_PATTERN_ON,																// Default | indicates busy
+	SYS_LED_PATTERN_SHORT, // 100ms on 900ms off									// Default | indicates waiting (pairing)
+	SYS_LED_PATTERN_LONG, // 500ms on 500ms off										// Default | indicates waiting
+	SYS_LED_PATTERN_FLASH, // 200ms on 200ms off									// Default | indicates readiness
 
-	SYS_LED_PATTERN_ONESHOT_POWERON, // 200ms on 200ms off, 3 times
-	SYS_LED_PATTERN_ONESHOT_POWEROFF, // 250ms off, 1000ms fade to off
-	SYS_LED_PATTERN_ONESHOT_PROGRESS, // 200ms on 200ms off, 2 times
-	SYS_LED_PATTERN_ONESHOT_COMPLETE, // 200ms on 200ms off, 4 times
+	SYS_LED_PATTERN_ONESHOT_POWERON, // 200ms on 200ms off, 3 times					// Default
+	SYS_LED_PATTERN_ONESHOT_POWEROFF, // 250ms off, 1000ms fade to off				// Default
+	SYS_LED_PATTERN_ONESHOT_PROGRESS, // 200ms on 200ms off, 2 times				// Success
+	SYS_LED_PATTERN_ONESHOT_COMPLETE, // 200ms on 200ms off, 4 times				// Success
 
-	SYS_LED_PATTERN_ON_PERSIST, // 20% duty cycle
-	SYS_LED_PATTERN_LONG_PERSIST, // 20% duty cycle, 500ms on 500ms off
-	SYS_LED_PATTERN_PULSE_PERSIST, // 5000ms pulsing
-	SYS_LED_PATTERN_ACTIVE_PERSIST, // 300ms on 9700ms off
+	SYS_LED_PATTERN_ON_PERSIST, // 20% duty cycle									// Success | indicates charged
+	SYS_LED_PATTERN_LONG_PERSIST, // 20% duty cycle, 500ms on 500ms off				// Error   | indicates low battery
+	SYS_LED_PATTERN_PULSE_PERSIST, // 5000ms pulsing								// Default | indicates charging
+	SYS_LED_PATTERN_ACTIVE_PERSIST, // 300ms on 9700ms off							// Default | indicates normal operation
 
-	SYS_LED_PATTERN_ERROR_A, // 500ms on 500ms off, 2 times, every 5000ms
-	SYS_LED_PATTERN_ERROR_B, // 500ms on 500ms off, 3 times, every 5000ms
-	SYS_LED_PATTERN_ERROR_C, // 500ms on 500ms off, 4 times, every 5000ms
-	SYS_LED_PATTERN_ERROR_D, // 500ms on 500ms off (same as SYS_LED_PATTERN_LONG)
+	SYS_LED_PATTERN_ERROR_A, // 500ms on 500ms off, 2 times, every 5000ms			// Error
+	SYS_LED_PATTERN_ERROR_B, // 500ms on 500ms off, 3 times, every 5000ms			// Error
+	SYS_LED_PATTERN_ERROR_C, // 500ms on 500ms off, 4 times, every 5000ms			// Error
+	SYS_LED_PATTERN_ERROR_D, // 500ms on 500ms off (same as SYS_LED_PATTERN_LONG)	// Error
 };
 
 void set_led(enum sys_led_pattern led_pattern, int priority);
